30a20741cbbcaa10f54b43986a13a451738e173b
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-20 02:05:53 +0300

    update settings filepath

--- core/application_engine/component_deploy.sh
+++ core/application_engine/component_deploy.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-host=$1
-db_name=$2
-db_user=$3
-db_pass=$4
+project=$1
+host=$2
+db_name=$3
+db_user=$4
+db_pass=$5
 
-sed -i -e "s/allow_pattern/'$host','www.$host'/g" ../settings.py >> /dev/null
+sed -i -e "s/allow_pattern/'$host','www.$host'/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
 
-sed -i -e "s/DB_NAME/$db_name/g" ../settings.py >> /dev/null
-sed -i -e "s/USER_NAME/$db_user/g" ../settings.py >> /dev/null
-sed -i -e "s/DB_PASSWORD/$db_pass/g" ../settings.py >> /dev/null
+sed -i -e "s/DB_NAME/$db_name/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
+sed -i -e "s/USER_NAME/$db_user/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
+sed -i -e "s/DB_PASSWORD/$db_pass/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
 
-rm -rf ./component_deploy.sh
+rm -rf /home/hotdog/$project/djapp/application_engine/component_deploy.sh

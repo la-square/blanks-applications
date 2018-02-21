0986637e8dc88abee4d7ded1637e778637bf0350
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-21 18:00:21 +0300

    rename core to engine

---
+++ engine/engine/component_deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+project=$1
+host=$2
+db_name=$3
+db_user=$4
+db_pass=$5
+
+sed -i -e "s/allow_pattern/'$host','www.$host'/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
+
+sed -i -e "s/DB_NAME/$db_name/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
+sed -i -e "s/USER_NAME/$db_user/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
+sed -i -e "s/DB_PASSWORD/$db_pass/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
+
+rm -rf /home/hotdog/$project/djapp/engine/component_deploy.sh

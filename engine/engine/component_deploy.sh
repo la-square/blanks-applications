874f8ac1fe99f3409d920d6ed9e84e186ac021ed
i.chelyshkin <i.chelyshkin@corp.mail.ru>
2018-02-25 14:16:38 +0300

    update stdout

--- engine/engine/component_deploy.sh
+++ engine/engine/component_deploy.sh
@@ -6,6 +6,12 @@ db_name=$3
 db_user=$4
 db_pass=$5
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+printf "${CYAN}Prepair settings.py${NC}\n"
 sed -i -e "s/allow_pattern/'$host','www.$host'/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
 
 sed -i -e "s/DB_NAME/$db_name/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
@@ -13,6 +19,3 @@ sed -i -e "s/USER_NAME/$db_user/g" /home/hotdog/$project/djapp/settings.py >> /d
 sed -i -e "s/DB_PASSWORD/$db_pass/g" /home/hotdog/$project/djapp/settings.py >> /dev/null
 
 rm -rf /home/hotdog/$project/djapp/engine/component_deploy.sh
-
-systemctl daemon-reload
-systemctl restart uwsgi_$project.service
